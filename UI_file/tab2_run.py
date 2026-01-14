fc0a13cdf0afded2661b952b05dfae3cfd5fac7e
jinxianwei <81373517+jinxianwei@users.noreply.github.com>
2026-01-14 14:56:36 +0800

    Create tab2_run.py

---
+++ UI_file/tab2_run.py
@@ -0,0 +1,24 @@
+import os
+import copy
+import csv
+from PyQt5.QtWidgets import QWidget, QFileDialog, QMessageBox
+from PyQt5 import QtCore
+from UI_file import tab2_ui
+
+# tab2
+class tab2Window(QWidget, tab2_ui.Ui_tab2_imageProcess):
+    def __init__(self):
+        super(tab2Window, self).__init__()
+        tab2_ui.Ui_tab2_imageProcess.__init__(self)
+        self.setupUi(self)
+        self.testBtn.clicked.connect(self.test_func)
+
+ 
+    def set_lib(self, lib, cur_ip, port, offline_flag):
+        self.device_lib = lib
+        self.machine_ip = cur_ip
+        self.port = port
+        self.offline_flag = offline_flag
+
+    def test_func(self):
+        print("测试tab2按钮被点击")
